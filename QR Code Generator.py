c8e2d1f2f6b3203320013139bdbf6fdd0232635a
Alfredo Herculano <33830688+alfredoherculano@users.noreply.github.com>
2022-11-16 16:48:14 -0300

    Add files via upload

---
+++ QR Code Generator.py
@@ -0,0 +1,21 @@
+# install the libraries needed
+# create a function that collects the text and converts it to a QR code
+# save the QR as an image
+# run the function
+
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("https://www.nasa.gov")
