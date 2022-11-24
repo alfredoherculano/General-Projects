cc7519d0104e682d93e268d3ec3408f529b9ed99
Alfredo Herculano <33830688+alfredoherculano@users.noreply.github.com>
2022-11-24 08:50:48 -0300

    Add files via upload

---
+++ Leap Year Checker.py
@@ -0,0 +1,15 @@
+input_year = int(input("Please provide an year: "))
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year!")
+            else:
+                print("Not a leap year.")
+        else:
+            print("Leap year!")
+    else:
+        print("Not a leap year.")
+
+is_leap_year(input_year)
